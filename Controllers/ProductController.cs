91c5e4777d92a194a4cb733946bbc17719d16ed1
Murtazoev Alijon <murtazoevalijon@gmail.com>
2024-07-18 01:03:37 +0300

    Products and Product Counter

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ProductController : Controller
+    {
+        static Product newProduct;
+
+        int cnt = 0;
+        public ProductController()
+        {
+            if (newProduct  == null)
+            {
+                newProduct = new Product();
+            }
+        }
+        public IActionResult createProduct()
+        {
+            newProduct.Name = "Salom Aleykum";
+            newProduct.ContactNumebr = "+99298asd123123";
+            newProduct.Info = "an information about the product";
+            return View(newProduct);
+        }
+        // [HttpPost]
+        public IActionResult Increase()
+        {
+            newProduct.Id++;
+            return RedirectToAction("createProduct");
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication2.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public List<string> images { get; set; }
+        public string Name { get; set; }
+        public string Info {  get; set; }
+        public string ContactNumebr { get; set; }
+
+    }
+}
